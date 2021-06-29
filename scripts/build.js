e208bbe66ad9cd9b1334444d201d897b67c4dc51
Elio Struyf <info@estruyf.be>
2021-06-29 11:01:52 +0200

    Updated build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const jsonc = require('jsonc');
+const jsonc = require('jsonc-parser');
 
 const commonDark = `../src/commonDark.jsonc`;
 const darkItalicFile = `TheUnnamed-dark-theme.json`;
@@ -8,12 +8,12 @@ const darkNoItalicFile = `TheUnnamed-dark-no-italics-theme.json`;
 
 (() => {
   const commonDarkContents = jsonc.parse(fs.readFileSync(path.join(__dirname, commonDark), { encoding: "utf-8" }));
-  const darkItalicContents = jsonc.parse(fs.readFileSync(path.join(__dirname, `../src/`, darkItalicFile), { encoding: "utf-8" }));
-  const darkNoItalicContents = jsonc.parse(fs.readFileSync(path.join(__dirname, `../src/`, darkNoItalicFile), { encoding: "utf-8" }));
+  const darkItalicContents = JSON.parse(fs.readFileSync(path.join(__dirname, `../src/`, darkItalicFile), { encoding: "utf-8" }));
+  const darkNoItalicContents = JSON.parse(fs.readFileSync(path.join(__dirname, `../src/`, darkNoItalicFile), { encoding: "utf-8" }));
 
   const darkItalic = { ...commonDarkContents, ...darkItalicContents };
   const darkNoItalic = { ...commonDarkContents, ...darkNoItalicContents };
 
-  fs.writeFileSync(path.join(__dirname, `../themes`, darkItalic), jsonc.stringify(dictionary, null, 2), { encoding: "utf-8" });
-  fs.writeFileSync(path.join(__dirname, `../themes`, darkNoItalic), jsonc.stringify(dictionary, null, 2), { encoding: "utf-8" });
+  fs.writeFileSync(path.join(__dirname, `../themes`, darkItalicFile), JSON.stringify(darkItalic, null, 2), { encoding: "utf-8" });
+  fs.writeFileSync(path.join(__dirname, `../themes`, darkNoItalicFile), JSON.stringify(darkNoItalic, null, 2), { encoding: "utf-8" });
 })();
