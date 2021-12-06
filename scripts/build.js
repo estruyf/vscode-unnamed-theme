4f735ee93cdbcd9693b857c548d5c6ff513d73ce
Elio Struyf <elio@struyfconsulting.be>
2021-12-06 16:56:02 +0100

    Light theme in progress

--- scripts/build.js
+++ scripts/build.js
@@ -6,6 +6,9 @@ const commonDark = `../src/commonDark.jsonc`;
 const darkItalicFile = `TheUnnamed-dark-theme.json`;
 const darkNoItalicFile = `TheUnnamed-dark-no-italics-theme.json`;
 
+const commonLight = `../src/commonLight.jsonc`;
+const lightItalicFile = `TheUnnamed-light-theme.json`;
+
 (() => {
   const commonDarkContents = jsonc.parse(fs.readFileSync(path.join(__dirname, commonDark), { encoding: "utf-8" }));
   const darkItalicContents = JSON.parse(fs.readFileSync(path.join(__dirname, `../src/`, darkItalicFile), { encoding: "utf-8" }));
@@ -16,4 +19,12 @@ const darkNoItalicFile = `TheUnnamed-dark-no-italics-theme.json`;
 
   fs.writeFileSync(path.join(__dirname, `../themes`, darkItalicFile), JSON.stringify(darkItalic, null, 2), { encoding: "utf-8" });
   fs.writeFileSync(path.join(__dirname, `../themes`, darkNoItalicFile), JSON.stringify(darkNoItalic, null, 2), { encoding: "utf-8" });
+
+
+  const commonLightContents = jsonc.parse(fs.readFileSync(path.join(__dirname, commonLight), { encoding: "utf-8" }));
+  const lightItalicContents = JSON.parse(fs.readFileSync(path.join(__dirname, `../src/`, lightItalicFile), { encoding: "utf-8" }));
+
+  const lightItalic = { ...commonLightContents, ...lightItalicContents };
+
+  fs.writeFileSync(path.join(__dirname, `../themes`, lightItalicFile), JSON.stringify(lightItalic, null, 2), { encoding: "utf-8" });
 })();
